51aff5f52b51c5092650e6012526a07447b06cce
Lucas Felipe Cortés Gutiérrez <lcortes@alumnos.inf.utfsm.cl>
2019-12-05 22:33:47 -0300

    Upload New File

---
+++ MeasureTactics.ino
@@ -0,0 +1,53 @@
+/**
+ * @function measureLine : Lee los sensores de línea y determina si se está detecctando un fondo blanco
+ * @param : none
+ * @return : retorna 0 si no se detecta una línea, 1 en caso contrario
+ */
+
+
+bool measureEnemy(){
+    enemy_sensor_left = !digitalRead(PIN_SENSOR_LEFT);
+    enemy_sensor_f_left = !digitalRead(PIN_SENSOR_F_LEFT);
+    enemy_sensor_front = !digitalRead(PIN_SENSOR_FRONT);
+    enemy_sensor_f_right = !digitalRead(PIN_SENSOR_F_RIGHT);
+    enemy_sensor_right = !digitalRead(PIN_SENSOR_RIGHT);
+
+    #if DEBUG > NONE
+        Serial.print("\n>>> LOG: mediciones de enemigo: \t");
+        Serial.print(enemy_sensor_left);
+        Serial.print("\t");
+        Serial.print(enemy_sensor_f_left);
+        Serial.print("\t");
+        Serial.print(enemy_sensor_front);
+        Serial.print("\t");
+        Serial.print(enemy_sensor_f_right);
+        Serial.print("\t");
+        Serial.print(enemy_sensor_right);
+    #endif
+        
+    return (enemy_sensor_left) || (enemy_sensor_f_left) || (enemy_sensor_front) || (enemy_sensor_f_right) || (enemy_sensor_right);
+}
+
+
+void rc_read_values() {
+    noInterrupts();
+    memcpy(rc_values, (const void *)rc_shared, sizeof(rc_shared));
+    interrupts();
+}
+
+
+void calc_input(uint8_t channel, uint8_t input_pin) {
+    if (digitalRead(input_pin) == HIGH) {
+        rc_start[channel] = micros();
+    } 
+    else {
+        uint16_t rc_compare = (uint16_t)(micros() - rc_start[channel]);
+        rc_shared[channel] = rc_compare;
+    }
+}
+
+
+void calc_ch1() { calc_input(DIRECTION, DIRECTION_INPUT); }
+void calc_ch2() { calc_input(THROTLE, THROTLE_INPUT); }
+void calc_ch3() { calc_input(SWITCH, SWITCH_INPUT); }
+void calc_ch4() { calc_input(BUTTON, BUTTON_INPUT); }
